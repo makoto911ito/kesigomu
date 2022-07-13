91740a1f17512c28ef8d3868f52b8b0f1e81bf71
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2022-07-13 17:31:53 +0900

    script変更

--- Assets/nagai/Scripts/Result.cs
+++ Assets/nagai/Scripts/Result.cs
@@ -6,9 +6,17 @@ public class Result : MonoBehaviour
 {
     private void Update()
     {
-        if()
+        if( == false)
         {
-
+            Debug.Log("plyer1‚ªŸ‚Á‚½");
+        }
+        else if(this.gameObject == false)
+        {
+            Debug.Log("plyer2‚ªŸ‚Á‚½");
+        }
+        else
+        {
+            Debug.Log("ˆø‚«•ª‚¯");
         }
     }
 }
