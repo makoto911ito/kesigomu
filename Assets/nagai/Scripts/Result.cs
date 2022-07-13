bd6afd9ada24894f954950b4c95fdb0c6dbe4482
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2022-07-13 16:01:40 +0900

    test

---
+++ Assets/nagai/Scripts/Result.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result : MonoBehaviour
+{
+    private void Update()
+    {
+        if()
+        {
+
+        }
+    }
+}
