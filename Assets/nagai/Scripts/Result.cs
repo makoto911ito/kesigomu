673ddf7f0982aa6ec2b8682aadd2143f2aaa2f39
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2022-07-29 11:40:20 +0900

    勝敗

--- Assets/nagai/Scripts/Result.cs
+++ Assets/nagai/Scripts/Result.cs
@@ -1,22 +1,26 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;    // PunTurnManager, IPunTurnManagerCallbacks を使うため
+using Photon.Realtime;
 
-//public class Result : MonoBehaviour
-//{
-//    private void Update()
-//    {
-//        if ( == true)
-//        {
-//            Debug.Log("plyer1が勝った");
-//        }
-//        else if ()
-//        {
-//            Debug.Log("plyer2が勝った");
-//        }
-//        else
-//        {
-//            Debug.Log("引き分け");
-//        }
-//    }
-//}
+public class Result : MonoBehaviour
+{
+    PhotonView _view;
+    private void Start()
+    {
+        _view = GetComponent<PhotonView>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            Debug.Log("プレイヤー1の勝ち");
+        }
+        else if (PhotonNetwork.LocalPlayer.IsLocal)
+        {
+            Debug.Log("プレイヤー２の勝ち");
+        }
+    }
+}
