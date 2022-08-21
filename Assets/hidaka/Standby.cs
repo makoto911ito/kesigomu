644848ef35955779b75a2893ef8f561326b390d2
ｍ <yasasinn65@gmail.com>
2022-08-21 22:56:25 +0900

    [Apdate]スポーンポイントのプレハブ化とgamemanagerへの再アタッチをしました。

--- Assets/hidaka/Standby.cs
+++ Assets/hidaka/Standby.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class Standby : MonoBehaviour
@@ -11,6 +12,9 @@ public class Standby : MonoBehaviour
     [SerializeField] GameObject wait = null;
     bool waitFlag = false; //待機画面を表示するか否か
     float time = 0; //点滅速度
+    [SerializeField] GameObject spornPoiunt;
+    List<GameObject> sp = new List<GameObject>();
+    GameObject[] pl;
     void Start()
     {
         wait = GetComponent<GameObject>();
@@ -22,13 +26,12 @@ public class Standby : MonoBehaviour
         time += Time.deltaTime;
 
         //スポーンポイントの数をMaxとする
-        GameObject[] sp = GameObject.FindGameObjectsWithTag("Untagged");
-        GameObject[] pl = GameObject.FindGameObjectsWithTag("Player");
-        memberText.text = "待機中のプレイヤー\n" + pl.Length +"/" + sp.Length;
-        playersName.text += "Player" + pl[pl.Length + 1] + ":" + pl[pl.Length].name + "\n";
+        //GameObject[] sp = GameObject.FindGameObjectsWithTag("Finish");
+        pl = GameObject.FindGameObjectsWithTag("Player");
+        
 
         //プレイヤーがスポーンポイント数に満たない場合
-        if (pl.Length == sp.Length) waitFlag = false;
+        if (pl.Length == sp.Count) waitFlag = false;
         else
         {
             waitFlag = true;
@@ -47,5 +50,11 @@ public class Standby : MonoBehaviour
         }
 
         //public void ChangeScene(Scene scene) => SceneManager.LoadScene("Title");
+    }
+    void Character()
+    {
+        memberText.text = "待機中のプレイヤー\n" + pl.Length + "/" + sp.Count;
+        playersName.text += "Player" + pl.Length + ":" + pl[pl.Length - 1].name + "\n";
+
     }
 }
