21cbe182e26018adccf3e7ade622d4866d33fea8
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2022-10-17 18:13:14 +0900

    アニメーションの追加、スクリプト追加

--- Assets/nagai/Scripts/TurnChange.cs
+++ Assets/nagai/Scripts/TurnChange.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class TurnChange : MonoBehaviour
 {
-    
+    [SerializeField] Animator _animator;
+
+    public void TurnChanger()
+    {
+        _animator.SetTrigger("Image");
+    }
 }
